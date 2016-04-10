2c40915e75de198bb73b481309861860d3d23d22
nal luksic <nl0243@student.uni-lj.si>
2016-04-10 19:36:54 +0000

    Zaključek dela na IV. nalogi

--- public/js/klepet.js
+++ public/js/klepet.js
@@ -43,10 +43,24 @@ Klepet.prototype.procesirajUkaz = function(ukaz) {
         sporocilo = 'Neznan ukaz';
       }
       break;
+    case 'dregljaj':
+      besede.shift();
+      var vzdevek = besede.join(' ');
+      if (vzdevek.length>0) 
+      {
+        this.socket.emit('dregljaj', {vzdevek: vzdevek});
+        sporocilo = 'Dregljaj za ' + vzdevek;
+      } 
+      else 
+      {
+        sporocilo = 'Neznan ukaz';
+      }
+      break; 
+      
     default:
       sporocilo = 'Neznan ukaz.';
       break;
-  };
-
+  }
+  
   return sporocilo;
 };
